af146ac008b559289c5e38439e3e68c64b6c5852
jinyuying <18234030399@163.com>
2021-06-02 19:01:52 +0800

    refactor: 修改配置

---
+++ scripts/jest.js
@@ -0,0 +1,11 @@
+const minimist = require('minimist')
+const rawArgs = process.argv.slice(2)
+const args = minimist(rawArgs)
+const path = require('path')
+let rootDir = path.resolve(__dirname, '../')
+// 指定包测试
+if (args.p) {
+  rootDir = rootDir + '/packages/' + args.p
+}
+const jestArgs = ['--runInBand', '--rootDir', rootDir]
+require('jest').run(jestArgs)
